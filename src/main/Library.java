3d54ae7cf336fce663ec16fcb983fc909a1f96e5
tadhgryan2012 <tadhgryan2012@gmail.com>
2022-11-11 23:41:01 +0000

    Done more stuff

--- src/main/Library.java
+++ src/main/Library.java
@@ -3,51 +3,14 @@ package main;
 import java.util.ArrayList;
 
 public class Library {
-	private ArrayList<Book> books;
-	private ArrayList<Loan> loans;
-	private ArrayList<User> users;
-	private ArrayList<Staff> staff;
+	private ArrayList<Department> deps;
 
-	public Library() {
-		this.books = new ArrayList<>();
-		this.loans = new ArrayList<>();
-		this.users = new ArrayList<>();
-		this.staff = new ArrayList<>();
+	public ArrayList<Department> getDeps() {
+		return deps;
 	}
 
-	public ArrayList<Book> getBooks() {
-		return books;
-	}
-
-	public void setBooks(ArrayList<Book> books) {
-		this.books = books;
-	}
-
-	public ArrayList<Loan> getLoans() {
-		return loans;
-	}
-
-	public void setLoans(ArrayList<Loan> loans) {
-		this.loans = loans;
-	}
-
-	public ArrayList<User> getUsers() {
-		return users;
-	}
-
-	public void setUsers(ArrayList<User> users) {
-		this.users = users;
-	}
-
-	public ArrayList<Staff> getStaff() {
-		return staff;
-	}
-
-	public void setStaff(ArrayList<Staff> staff) {
-		this.staff = staff;
+	public void setDeps(ArrayList<Department> deps) {
+		this.deps = deps;
 	}
 	
-	public void borrow(Library lib, Book book) {
-
-	}
 }
---
+++ src/test/LibraryTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibraryTest {
+	/*
+	 * Unnecessary duplicate acquisition, by several departments, of infrequently accessed
+	 * copies of books or proceedings that are relevant to more than one department.
+	 */
+
+	/* Unnecessary subscription by several departments to expensive journals that are
+	 * relevant to more than one department. 
+	 */
+
+	/* Acquisition of books or proceedings of marginal interest to the university, which
+	 * could be borrowed from other universities with which UWON has an agreement 
+	 */
+	
+	/* Subscription to journals of marginal interest to the university, which could be
+	 * accessed in other universities with which UWON has an agreement.
+ 	 */
+
+	/* Unavailability of requested books, for a variety of reasons such as department
+	 * budget restrictions, excessive borrowing by the same user, lack of enforcement of
+	 * rules limiting loan periods, loss or stealing of book copies and so on. 
+	 */
+
+	/* Unavailability of journal issues while they are being bound into yearly volumes 
+	 */
+
+	/* Lack of traceability to previous borrowers when books, proceedings or journal
+	 * volumes are found to be damaged 
+	 */
+
+	/* Inaccuracy of card indexes, e.g. a book is stated as being available whereas it is not
+	 * found at the appropriate place on the shelves.
+ 	 */
+
+	/* Bibliographical search restricted to library opening hours. Slow, tedious
+	 * bibliographical search due to manipulation of card indexes.
+	 */
+	
+	/* Inaccurate search results, due to poor classification of books, journals or
+	 * proceedings within departments 
+	 */
+
+	/* Incomplete or ineffective search results, due to relevant books, journals or
+	 * proceedings being indexed in other UWON department libraries, or unavailable at
+	 * UWON.
+	 */
+	
+}
