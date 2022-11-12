0059911cbf54f2fecef3e872456eaf63f0e4747f
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-12 16:12:22 +0000

    Update LibraryTest.java

--- src/test/LibraryTest.java
+++ src/test/LibraryTest.java
@@ -1,16 +1,9 @@
-package test;
+package org.example;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
-
-import main.Book;
-import main.Department;
-import main.Library;
-import main.Loan;
-import main.Subscription;
-import main.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,107 +11,152 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/*import main.Book;
+import main.Department;
+import main.Library;
+import main.Loan;
+import main.Subscription;
+import main.User;*/
+
 public class LibraryTest {
-	/*
-	 * Unnecessary duplicate acquisition, by several departments, of infrequently accessed
-	 * copies of books or proceedings that are relevant to more than one department.
-	 */
+    /* 
+     * Unnecessary duplicate acquisition, by several departments, of infrequently accessed
+     * copies of books or proceedings that are relevant to more than one department.
+     */
 
-	/* Unnecessary subscription by several departments to expensive journals that are
-	 * relevant to more than one department.
-	 */
+    /* Unnecessary subscription by several departments to expensive journals that are
+     * relevant to more than one department.
+     */
+    @Test
+    @DisplayName("Unnecessary subscriptions test")
+    public void unnecessarySubTest() {
+        Library UL = new Library();
+        Library UCC = new Library();
 
-	/* Acquisition of books or proceedings of marginal interest to the university, which
-	 * could be borrowed from other universities with which UWON has an agreement.
-	 */
-	@Test
-	@DisplayName("University sharing books Test")
-	public void shareBooksTest() {
-		Library UL = new Library();
-		Library UCC = new Library();
+        ArrayList<Book> books = new ArrayList<>(Arrays.asList(
+                new Book("A Game of Thrones", "George R. R. Martin", "Fantasy"),
+                new Book("A clash of Kings", "George R. R. Martin", "Fantasy"),
+                new Book("A Storm of Swords", "George R. R. Martin", "Fantasy")
+        ));
 
-		Book book = new Book("Harry Potter and the Goblet of Fire", "J.K. Rowling", "Action");
-		ArrayList<Book> books = new ArrayList<>();
-		books.add(book);
+        Subscription subs = new Subscription("Game of Thrones Trilogy", books);
 
-		UL.addBook(book);
-		UL.addAggreement(UCC);
-		UCC.borrowBook(UL, book);
-		assertEquals(books, UCC.getBooks());
-	}
-	
-	/* Subscription to journals of marginal interest to the university, which could be
-	 * accessed in other universities with which UWON has an agreement.
- 	 */
-	@Test
-	@DisplayName("Share Subscriptions Test")
-	public void shareSubscriptionsTest() {
-		Library UL = new Library();
-		Library UCC = new Library();
+        UL.addSub(subs);
+        assertNotEquals(UL.getSubs (), UCC.getSubs());
+        UL.addAggreement(UCC);
+        UL.shareSub(UCC, subs);
+        assertEquals(UL.getSubs(), UCC.getSubs());
+    }
 
-		ArrayList<Book> books = new ArrayList<>(Arrays.asList(
-			new Book("Shrek", "William Steig", "Comedy"),
-			new Book("Shrek 2", "William Steig", "Comedy"),
-			new Book("Shrek 3", "William Steig", "Comedy")
-		));
+    /* Acquisition of books or proceedings of marginal interest to the university, which
+     * could be borrowed from other universities with which UWON has an agreement
+     */
+    @Test
+    @DisplayName("University sharing books Test")
+    public void shareBooksTest() {
+        Library UL = new Library();
+        Library UCC = new Library();
 
-		Subscription sub = new Subscription("Netflix", books);
+        Book book = new Book("Harry Potter and the Goblet of Fire", "J.K. Rowling", "Action");
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(book);
 
-		UL.addSub(sub);
-		UL.addAggreement(UCC);
-		UL.shareSub(UCC, sub);
-		assertEquals(UL.getSubs(), UCC.getSubs());
-	}
+        UL.addBook(book);
+        UL.addAggreement(UCC);
+        UCC.borrowBook(UL, book);
+        assertEquals(books, UCC.getBooks());
+    }
 
-	/* Unavailability of requested books, for a variety of reasons such as department
-	 * budget restrictions, excessive borrowing by the same user, lack of enforcement of
-	 * rules limiting loan periods, loss or stealing of book copies and so on. 
-	 */
+    /* Subscription to journals of marginal interest to the university, which could be
+     * accessed in other universities with which UWON has an agreement.
+     */
+    @Test
+    @DisplayName("Share Subscriptions Test")
+    public void shareSubscriptionsTest() {
+        Library UL = new Library();
+        Library UCC = new Library();
 
-	/* Unavailability of journal issues while they are being bound into yearly volumes.
-	 */
+        ArrayList<Book> books = new ArrayList<>(Arrays.asList(
+                new Book("Shrek", "William Steig", "Comedy"),
+                new Book("Shrek 2", "William Steig", "Comedy"),
+                new Book("Shrek 3", "William Steig", "Comedy")
+        ));
 
-	/* Lack of traceability to previous borrowers when books, proceedings or journal
-	 * volumes are found to be damaged.
-	 */
-	@Test
-	@DisplayName("Show History of Ownership Test")
-	public void traceabilityTest() {
-		Department CSIS = new Department();
+        Subscription sub = new Subscription("Netflix", books);
 
-		User john = new User();
-		User milan = new User();
-		Book book = new Book("Mein Kampf", "Adolf Hitler", "Gospel");
+        UL.addSub(sub);
+        UL.addAggreement(UCC);
+        UL.shareSub(UCC, sub);
+        assertEquals(UL.getSubs(), UCC.getSubs());
+    }
 
-		CSIS.loan(book, LocalDate.now().minusDays(15), john);
-		CSIS.returnLoan(book, LocalDate.now().minusDays(10), john);
+    /* Unavailability of requested books, for a variety of reasons such as department
+     * budget restrictions, excessive borrowing by the same user, lack of enforcement of
+     * rules limiting loan periods, loss or stealing of book copies and so on.
+     */
 
-		CSIS.loan(book, LocalDate.now().minusDays(5), milan);
-		CSIS.returnLoan(book, LocalDate.now(), milan);
+    /* Unavailability of journal issues while they are being bound into yearly volumes
+     */
 
-		ArrayList<Loan> expectedResults = new ArrayList<>(Arrays.asList(
-			new Loan(book, LocalDate.now().minusDays(15), john), 
-			new Loan(book, LocalDate.now().minusDays(5), milan)
-		));
+    /* Lack of traceability to previous borrowers when books, proceedings or journal
+     * volumes are found to be damaged
+     */
+    @Test
+    @DisplayName("Show History of Ownership Test")
+    public void traceabilityTest() {
+        Department CSIS = new Department();
 
-		assertTrue(expectedResults.toString().equals(CSIS.getHistoryOfBook(book).toString()));		
-	}
+        User john = new User();
+        User milan = new User();
+        Book book = new Book("Mein Kampf", "Adolf Hitler", "Gospel");
 
-	/* Inaccuracy of card indexes, e.g. a book is stated as being available whereas it is not
-	 * found at the appropriate place on the shelves.
- 	 */
+        CSIS.loan(book, LocalDate.now().minusDays(15), john);
+        CSIS.returnLoan(book, LocalDate.now().minusDays(10), john);
 
-	/* Bibliographical search restricted to library opening hours. Slow, tedious
-	 * bibliographical search due to manipulation of card indexes.
-	 */
-	
-	/* Inaccurate search results, due to poor classification of books, journals or
-	 * proceedings within departments.
-	 */
+        CSIS.loan(book, LocalDate.now().minusDays(5), milan);
+        CSIS.returnLoan(book, LocalDate.now(), milan);
 
-	/* Incomplete or ineffective search results, due to relevant books, journals or
-	 * proceedings being indexed in other UWON department libraries, or unavailable at
-	 * UWON.
-	 */
-	
+        ArrayList<Loan> expectedResults = new ArrayList<>(Arrays.asList(
+                new Loan(book, LocalDate.now().minusDays(15), john),
+                new Loan(book, LocalDate.now().minusDays(5), milan)
+        ));
+
+        assertTrue(expectedResults.toString().equals(CSIS.getHistoryOfBook(book).toString()));
+    }
+
+    /* Inaccuracy of card indexes, e.g. a book is stated as being available whereas it is not
+     * found at the appropriate place on the shelves.
+     */
+    @Test
+    @DisplayName("Card index test")
+    public void cardIndexTest() {
+        Department CSIS = new Department();
+        User john = new User();
+        User milan = new User();
+
+        Book book = new Book("The Fellowship of the Ring", "J. R. R. Tolkien", "Fantasy");
+        CSIS.loan(book, LocalDate.now().minusDays(15), john);
+        //Book not available therefore it should not allow Milan to loan it.
+        assertFalse(CSIS.loan(book, LocalDate.now().minusDays(15), milan));
+        CSIS.returnLoan(book, LocalDate.now(), john);
+        //book now available so it should assert true as john returned the loan.
+        assertTrue(CSIS.loan(book, LocalDate.now().minusDays(15), milan));
+
+    }
+
+
 }
+
+    /* Bibliographical search restricted to library opening hours. Slow, tedious
+     * bibliographical search due to manipulation of card indexes.
+     */
+
+    /* Inaccurate search results, due to poor classification of books, journals or
+     * proceedings within departments
+     */
+
+    /* Incomplete or ineffective search results, due to relevant books, journals or
+     * proceedings being indexed in other UWON department libraries, or unavailable at
+     * UWON.
+     */
+
