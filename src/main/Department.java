9247f7b55b2e8c25fdf48c1d6b3d982c33056f0b
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-13 21:14:55 +0000

    Add files via upload

--- src/main/Department.java
+++ src/main/Department.java
@@ -1,54 +1,64 @@
-package main;
+package test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Department {
-	private ArrayList<Loan> loans;
-	private ArrayList<User> users;
-	private ArrayList<Staff> staff;
+    private ArrayList<Loan> loans;
+    private ArrayList<User> users;
+    private ArrayList<Staff> staff;
 
-	public Department() {
-		loans = new ArrayList<>();
-		users = new ArrayList<>();
-		staff = new ArrayList<>();
-	}
+    public Department() {
+        loans = new ArrayList<>();
+        users = new ArrayList<>();
+        staff = new ArrayList<>();
+    }
 
-	public boolean loan(Book book, LocalDate date, User user) {
-		Loan loan = new Loan(book, date, user);
-		if (loans.contains(loan)) return false;
-		book.loan(loan);
-		loans.add(loan);
-		return true;
-	}
-	public boolean returnLoan(Book book, LocalDate date, User user) {
-		return loans.remove(new Loan(book, date, user));
-	}
-	public ArrayList<Loan> getHistoryOfBook(Book book) {
-		return book.getHistory();
-	}
+    public boolean loan(Book book, LocalDate date, User user) {
+        for (int i = 0; i < loans.size(); i++) {
+            if (loans.get(i).getBook() == book) return false;
+        }
+        Loan loan = new Loan(book, date, user);
+        book.loan(loan);
+        loans.add(loan);
+        return true;
+    }
 
-	public ArrayList<Loan> getLoans() {
-		return loans;
-	}
 
-	public void setLoans(ArrayList<Loan> loans) {
-		this.loans = loans;
-	}
+    public boolean returnLoan(Book book, LocalDate date, User user) {
+        for (int i = 0; i < loans.size(); i++) {
+            if (loans.get(i).getBook() == book && loans.get(i).getUser() == user) {
+                return loans.remove(loans.get(i));
+            }
+        }
+        return false;
+    }
 
-	public ArrayList<User> getUsers() {
-		return users;
-	}
+        public ArrayList<Loan> getHistoryOfBook (Book book){
+            return book.getHistory();
+        }
 
-	public void setUsers(ArrayList<User> users) {
-		this.users = users;
-	}
+        public ArrayList<Loan> getLoans () {
+            return loans;
+        }
 
-	public ArrayList<Staff> getStaff() {
-		return staff;
-	}
+        public void setLoans (ArrayList < Loan > loans) {
+            this.loans = loans;
+        }
 
-	public void setStaff(ArrayList<Staff> staff) {
-		this.staff = staff;
-	}
-}
+        public ArrayList<User> getUsers () {
+            return users;
+        }
+
+        public void setUsers (ArrayList < User > users) {
+            this.users = users;
+        }
+
+        public ArrayList<Staff> getStaff () {
+            return staff;
+        }
+
+        public void setStaff (ArrayList < Staff > staff) {
+            this.staff = staff;
+        }
+    }
