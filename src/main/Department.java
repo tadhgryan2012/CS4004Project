9b2e6efaff63c1e2a6d4e0db52689ed15286963a
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-15 00:20:37 +0000

    Update Department.java

--- src/main/Department.java
+++ src/main/Department.java
@@ -15,15 +15,24 @@ public class Department {
 	}
 
 	public boolean loan(Book book, LocalDate date, User user) {
-		Loan loan = new Loan(book, date, user);
-		if (loans.contains(loan)) return false;
-		book.loan(loan);
-		loans.add(loan);
-		return true;
-	}
-	public boolean returnLoan(Book book, LocalDate date, User user) {
-		return loans.remove(new Loan(book, date, user));
-	}
+        for (int i = 0; i < loans.size(); i++) {
+            if (loans.get(i).getBook().getId() == book.getId()) return false;
+        }
+        Loan loan = new Loan(book, date, user);
+        book.loan(loan);
+        loans.add(loan);
+        return true;
+   	}
+	
+    	public boolean returnLoan(Book book, LocalDate date, User user) {
+        for (int i = 0; i < loans.size(); i++) {
+            if (loans.get(i).getBook() == book && loans.get(i).getUser() == user) {
+                return loans.remove(loans.get(i));
+            }
+        }
+        return false;
+   	}
+	
 	public ArrayList<Loan> getHistoryOfBook(Book book) {
 		return book.getHistory();
 	}
