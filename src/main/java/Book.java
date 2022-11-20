1393a83708e5645c258cab1860b4d942b34598bb
tadhgryan2012 <tadhgryan2012@gmail.com>
2022-11-20 14:09:28 +0000

    Yuh

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -23,9 +23,9 @@ public class Book {
 		return historyOfLoans;
 	}
 
-//	public int getId() {
-//		return id;
-//	}
+	public int getId() {
+		return id;
+	}
 //
 //	public void setId(int id) {
 //		this.id = id;
